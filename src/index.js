a3a621d2db303a0021c53f36fc007551db38c5e8
andre ferrer <andrerferrer@hotmail.com>
2020-12-12 07:10:51 -0300

    Finish livecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello from src/index.js!");
+
+
+// 1. select
+const button = document.querySelector('#click-me');
+
+// console.log(button)
+// console log driven development
+
+// 2. add event listener
+// button.addEventListener('EVENT', BEHAVIOR)
+// button.addEventListener('click', () => {})
+button.addEventListener('click', (event) => {
+	// console.log(event);
+	// 3. change the inner text of the btn
+
+	// console.log(event.currentTarget);
+	// console.log(button);
+
+	button.innerText = 'clicked';
+});
